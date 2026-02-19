71c66b70ddcd324be1a0ffe26f712370134905a6
tatisneakers02-star <tatisneakers02@gmail.com>
2026-02-19 09:52:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+import discord
+from discord.ext import commands
 
+# Configuramos el bot
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.command()
+async def vgg(ctx, region: str, precio: float):
+    region = region.lower()
+    if region == "com":
+        payout = precio * 0.879
+        buyer = precio * 1.20
+        msg = f"✅ **VGG .COM**\n💰 Payout: {payout:.2f}$\n🛒 Buyer: {buyer:.2f}$"
+    elif region == "uk":
+        payout = precio * 0.85 
+        buyer = precio * 1.15
+        msg = f"✅ **VGG .UK**\n💰 Payout: {payout:.2f}£\n🛒 Buyer: {buyer:.2f}£"
+    else:
+        msg = "❌ Usa: !vgg com 100"
+    
+    await ctx.send(msg)
+
+# ABAJO PON TU TOKEN ENTRE LAS COMILLAS
+bot.run('MTQ3Mzk1NzY2OTc3MjQ2MDA0Mw.GDRUgm.Hn-VIFQlckzj3WrhgjNMOP74PHzm5z_fQ98nLI')
